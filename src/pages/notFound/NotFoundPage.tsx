300a3b4fe0e1cd8db1f5751d73f314e72108e7e9
xodn09 <a01049977214@gmail.com>
2026-07-06 21:26:35 +0900

    refactor: 관리자 404 페이지 이동 UX 개선

--- src/pages/notFound/NotFoundPage.tsx
+++ src/pages/notFound/NotFoundPage.tsx
@@ -1,15 +1,41 @@
+import { useLocation, useNavigate } from 'react-router-dom'
 import * as S from './NotFoundPage.styles'
 
 function NotFoundPage() {
+  const location = useLocation()
+  const navigate = useNavigate()
+  const requestedPath = `${location.pathname}${location.search}${location.hash}`
+
+  const handleGoBack = () => {
+    if (window.history.length > 1) {
+      navigate(-1)
+
+      return
+    }
+
+    navigate('/places', { replace: true })
+  }
+
   return (
     <S.Page>
       <S.Content>
+        <S.IconBadge aria-hidden="true">error_outline</S.IconBadge>
         <S.StatusCode>404</S.StatusCode>
         <S.Title>페이지를 찾을 수 없습니다.</S.Title>
         <S.Description>
-          요청한 관리자 페이지가 없거나 이동된 경로입니다.
+          요청한 관리자 페이지가 없거나 주소가 잘못 입력되었습니다. 입력한
+          경로를 확인하거나 이전 화면으로 돌아가 주세요.
         </S.Description>
-        <S.NavLink to="/main">메인으로 이동</S.NavLink>
+        <S.RequestPath>
+          <span>요청 경로</span>
+          <strong>{requestedPath}</strong>
+        </S.RequestPath>
+        <S.ActionGroup>
+          <S.BackButton type="button" onClick={handleGoBack}>
+            이전 페이지로
+          </S.BackButton>
+          <S.NavLink to="/places">장소 관리로 이동</S.NavLink>
+        </S.ActionGroup>
       </S.Content>
     </S.Page>
   )

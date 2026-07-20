5a4356ffe6c576e8b36b53d0c19b21ec84c72a00
xodn09 <a01049977214@gmail.com>
2026-07-20 20:50:42 +0900

    feat: 관리자 대시보드 라우트 추가

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -7,6 +7,7 @@ import {
   useLocation,
 } from 'react-router-dom'
 import LoginPage from '../../pages/login/LoginPage'
+import DashboardPage from '../../pages/dashboard/DashboardPage'
 import MainPage from '../../pages/main/MainPage'
 import NotFoundPage from '../../pages/notFound/NotFoundPage'
 import PlaceManagePage from '../../pages/place/PlaceManagePage'
@@ -45,6 +46,7 @@ export function Router() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/main" element={<MainPage />} />
           <Route path="/places" element={<PlaceManagePage />} />
           <Route path="/places/duplicates" element={<PlaceMergePage />} />

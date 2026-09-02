1edca913cd0f7c2919f749ab84fe246d98fe8d37
xodn09 <a01049977214@gmail.com>
2026-09-02 23:55:39 +0900

    refactor: 상점주 예약 메시지 자동 소거 적용

--- src/hooks/useMerchantReservationProducts.ts
+++ src/hooks/useMerchantReservationProducts.ts
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
+import { useAutoDismissMessage } from './useAutoDismissMessage'
 import {
   activateMerchantReservableProduct,
   createMerchantReservableProduct,
@@ -39,7 +40,9 @@ export function useMerchantReservationProducts() {
   const [errorMessage, setErrorMessage] = useState('')
   const [sectionErrorMessage, setSectionErrorMessage] = useState('')
   const [actionErrorMessage, setActionErrorMessage] = useState('')
+  useAutoDismissMessage(actionErrorMessage, setActionErrorMessage)
   const [successMessage, setSuccessMessage] = useState('')
+  useAutoDismissMessage(successMessage, setSuccessMessage)
   const [activeAction, setActiveAction] = useState<ProductAction>(null)
   const mountedRef = useRef(true)
   const actionRef = useRef<ProductAction>(null)
